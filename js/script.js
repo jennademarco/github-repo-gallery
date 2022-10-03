03f1c74e24e41c5fe389ee13ad40269e02648f20
jennademarco <jennaleedemarco@gmail.com>
2022-10-03 12:37:11 -0400

    Added repos to page via API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Profile information selector
 const overview = document.querySelector(".overview");
 const username = "jennademarco";
+const repoList = document.querySelector(".repo-list");
+
 const gitUserInfo = async function () {
     const userInfo = await fetch (`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
@@ -23,4 +25,20 @@ const displayUserInfo = function (data) {
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(div);
+  gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
